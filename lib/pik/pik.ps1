21d560e791a2b02ab3b77652e5c238757573dcf5
pekepeke <pekepekesamurai@gmail.com>
2013-03-21 10:38:11 +0900

    add autoinstall apps

--- lib/pik/pik.ps1
+++ lib/pik/pik.ps1
@@ -1,3 +1,3 @@
-pik_runner.exe $MyInvocation.MyCommand.Definition $args
-if(test-path -path "$ENV:HOME\.pik\pik_run.ps1"){& $ENV:HOME\.pik\pik_run.ps1}
-
+@ECHO OFF
+"%~dp0pik_runner.exe" "%~f0" %*
+IF EXIST "%USERPROFILE%\.pik\pik.bat" (call "%USERPROFILE%\.pik\pik.bat")
