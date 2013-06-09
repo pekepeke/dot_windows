8f64bc3c7bbd22230c02d2670d1d2463002d0539
pekepekesamurai <pekepekesamurai@gmail.com>
2013-06-09 12:06:15 +0900

    rename commands

--- setup.bat
+++ setup.bat
@@ -1,5 +1,9 @@
 @echo OFF
 
+if not exist %ChocolateyInstall% (
+	@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('http://chocolatey.org/install.ps1'))"
+	cinst "git"
+)
 if not defined HOME (
 	echo env ^%HOME^%: not found
 	goto EOF
@@ -8,8 +12,12 @@ if not exist %HOME%\.windows (
 	git clone --depth=1 git://github.com/pekepeke/dot_windows.git %HOME%\.windows
 	cd "%HOME%\.windows"
 	git submodule update --init
+) else (
+	git pull --rebase
+	git submodule update --init
 )
 
 goto EOF
 
 :EOF
+pause
