549c2c21d591dc8157c5fa400699960c97f12654
pekepeke <pekepekesamurai+github@gmail.com>
2015-07-30 18:52:03 +0900

    update script

--- bin/ntask.bat
+++ bin/ntask.bat
@@ -1,22 +1,23 @@
 @echo OFF
 set ROOT=%USERPROFILE%\デスクトップ
+if not exist %ROOT% set ROOT=%USERPROFILE%\Desktop
 set DD=%DATE:/=%
 set DD=%DD:~2,6%
-set NAME=%1
+set NAME=%*
 if "%NAME%"=="" set NAME=xxx
-set _FOLDER=%ROOT%\%DD%_%NAME%
-set FOLDER=%_FOLDER%
+set _FOLDER="%ROOT%\%DD%_%NAME%"
+set FOLDER="%_FOLDER%"
 set NUM=0
 set DEBUG=
 
 :CHECK
 if exist "%FOLDER%" (
 	set /A NUM=%NUM%+1
-	set FOLDER=%_FOLDER%_%NUM%
+	set FOLDER="%_FOLDER%_%NUM%"
 	goto CHECK
 )
 
 %DEBUG% mkdir "%FOLDER%"
-%DEBUG% type nul> "%FOLDER%\01_request.txt"
-%DEBUG% type nul> "%FOLDER%\02_memo.txt"
+%DEBUG% type nul> "%FOLDER%\01_request.md"
+%DEBUG% type nul> "%FOLDER%\02_memo.md"
 
